6fe3a6711e8fb7e1a618e4f6bd15787804c54cf6
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-15 15:55:46 -0300

    notification-service

---
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,42 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Get Recipient Notifications', () => {
+  it('should be able to get recipient notifications', async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationRepository,
+    );
+
+    await notificationRepository.create(
+      makeNotification({
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationRepository.create(
+      makeNotification({
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationRepository.create(
+      makeNotification({
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
